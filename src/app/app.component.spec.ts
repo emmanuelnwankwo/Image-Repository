e2bbfa7b20c25db346605717c4b73f1b77dcde8f
Emmanuel Nwankwo <Emmanuel.nwankwo@wemabank.com>
2021-01-13 23:51:07 +0100

    Update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'ImageRespository'`, async(() => {
+  it(`should have as title 'ImageRepository'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ImageRespository');
+    expect(app.title).toEqual('ImageRepository');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ImageRespository!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ImageRepository!');
   }));
 });
